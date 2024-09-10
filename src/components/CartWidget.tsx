e95b2210afbf63c606348450e686361db439fe03
Gferracioli <gustavo.freelancer.site@gmail.com>
2024-09-10 15:14:56 -0300

    corrigido o widget do carrinho

--- src/components/CartWidget.tsx
+++ src/components/CartWidget.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { TiShoppingCart } from "react-icons/ti";
-
+import { useCart } from "../context/CartContext"; // Importa o contexto do carrinho
 
 const CartWidget = () => {
+  const { cart } = useCart(); // Acessa o estado do carrinho
+
+  // Calcula a quantidade total de itens no carrinho
+  const totalItems = cart.reduce((acc, item) => acc + item.quantidade, 0);
+
   return (
     <div className="relative">
       <Link to="/cart">
         <TiShoppingCart className="text-white w-8 h-8" />
       </Link>
 
-      
-      <span className="absolute top-[-10px] right-[-10px] bg-red-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
-        7 {/* trocar por uma função que conte os items do carrinho quando estiver pronto */}
-      </span>
+      {totalItems > 0 && (
+        <span className="absolute top-[-10px] right-[-10px] bg-red-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+          {totalItems}
+        </span>
+      )}
     </div>
   );
 };
