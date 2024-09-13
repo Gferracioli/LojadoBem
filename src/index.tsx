fdcf690fe0bea9220ec55efee17346fb0c137f8a
Gferracioli <gustavo.freelancer.site@gmail.com>
2024-09-13 12:40:49 -0300

    rodapé do cart corrigido

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,7 @@ import Cart from "./pages/cart";
 import { LoadingProvider } from "../src/context/LoadingContex"; // Importa o hook do contexto de Loading
 import { CartProvider } from "../src/context/CartContext"; // importa cart
 import PaginaInstitucional from "./pages/AboutUs";
-import Footer from "./components/footer";
-
+import Footer from "./components/Footer";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -25,7 +24,7 @@ root.render(
           <Route path="/categoria/:id" element={<Home />} />
           <Route path="/item/:id" element={<Detalhes />} />
           <Route path="/cart" element={<Cart />} />
-          <Route path="/oprojeto" element= {<PaginaInstitucional />} />
+          <Route path="/oprojeto" element={<PaginaInstitucional />} />
         </Routes>
         <Footer />
       </LoadingProvider>
--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -5,7 +5,6 @@ import { useCart } from "../../context/CartContext";
 const Cart = () => {
   const navigate = useNavigate();
   const { cart, removeItem, clearCart, updateItemQuantity } = useCart();
- 
 
   // Calcular o total da compra
   const calcularTotal = () => {
@@ -14,72 +13,85 @@ const Cart = () => {
 
   // Renderização do carrinho de compras
   return (
-    <div className="container mx-auto p-8">
-      <button
-        onClick={() => navigate(-1)}
-        className="mb-4 px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition"
-      >
-        Voltar
-      </button>
-      <h1 className="text-3xl font-bold mb-6">Carrinho de Compras</h1>
-      {/*Verifica se o carrinho está vazio */}
-      {cart.length === 0 ? (
-        <p className="text-center">Seu carrinho está vazio.</p>
-      ) : (
-        <div className="bg-white p-4 rounded-lg shadow-md">
-          {cart.map((item) => (
-            <div
-              key={item.id}
-              className="flex justify-between items-center border-b py-4"
-            >
-              <div>
-                <h2 className="text-lg font-bold">{item.nome}</h2>
-                <p className="text-sm text-gray-600">Preço: R$ {item.price}</p>
-                <div className="flex items-center">
+    <div className="flex flex-col min-h-screen">
+      <div className="container mx-auto p-8">
+        <button
+          onClick={() => navigate(-1)}
+          className="mb-4 px-4 py-2 bg-gray-300 text-gray-800 rounded hover:bg-gray-400 transition"
+        >
+          Voltar
+        </button>
+        <h1 className="text-3xl font-bold mb-6">Carrinho de Compras</h1>
+        {/*Verifica se o carrinho está vazio */}
+        {cart.length === 0 ? (
+          <p className="text-center">Seu carrinho está vazio.</p>
+        ) : (
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            {cart.map((item) => (
+              <div
+                key={item.id}
+                className="flex justify-between items-center border-b py-4"
+              >
+                <div>
+                  <h2 className="text-lg font-bold">{item.nome}</h2>
+                  <p className="text-sm text-gray-600">
+                    Preço: R$ {item.price}
+                  </p>
+                  <div className="flex items-center">
+                    <button
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantidade - 1)
+                      }
+                      className="px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 transition"
+                      disabled={item.quantidade <= 1} // Não permitir que vá abaixo de 1
+                    >
+                      -
+                    </button>
+                    <span className="mx-3 text-gray-600">
+                      {item.quantidade}
+                    </span>
+                    <button
+                      onClick={() =>
+                        updateItemQuantity(item.id, item.quantidade + 1)
+                      }
+                      className="px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 transition"
+                      disabled={item.quantidade >= item.estoque}
+                    >
+                      +
+                    </button>
+                  </div>
+                  <p className="text-sm text-gray-600">
+                    Quantidade: {item.quantidade}
+                  </p>{" "}
+                  {/* Substitui estoque com quantidade correta */}
+                </div>
+                <div>
                   <button
-                    onClick={() => updateItemQuantity(item.id, item.quantidade - 1)}
-                    className="px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 transition"
-                    disabled={item.quantidade <= 1} // Não permitir que vá abaixo de 1
+                    onClick={() => removeItem(item.id)}
+                    className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
                   >
-                    -
-                  </button>
-                  <span className="mx-3 text-gray-600">{item.quantidade}</span>
-                  <button
-                    onClick={() => updateItemQuantity(item.id, item.quantidade + 1)}
-                    className="px-3 py-1 bg-gray-200 text-gray-700 rounded hover:bg-gray-300 transition" disabled={item.quantidade >= item.estoque}
-                  >
-                    +
+                    Remover
                   </button>
                 </div>
-                <p className="text-sm text-gray-600">
-                  Quantidade: {item.quantidade}
-                </p>{" "}
-                {/* Substitui estoque com quantidade correta */}
               </div>
-              <div>
-                <button
-                  onClick={() => removeItem(item.id)}
-                  className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 transition"
-                >
-                  Remover
-                </button>
-              </div>
+            ))}
+            <div className="mt-6">
+              <h2 className="text-2xl font-bold">
+                Total: R$ {calcularTotal()}
+              </h2>
             </div>
-          ))}
-          <div className="mt-6">
-            <h2 className="text-2xl font-bold">Total: R$ {calcularTotal()}</h2>
+            {/* limpar o carrinho */}
+            <div className="mt-4">
+              <button
+                onClick={clearCart}
+                className="px-6 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition"
+              >
+                Limpar Carrinho
+              </button>
+            </div>
           </div>
-          {/* limpar o carrinho */}
-          <div className="mt-4">
-            <button
-              onClick={clearCart}
-              className="px-6 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition"
-            >
-              Limpar Carrinho
-            </button>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
